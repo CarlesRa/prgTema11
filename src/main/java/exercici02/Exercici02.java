573106736e8d4c6d8d5572505108e4f103f4aed0
Carlesra <juan.ramos.a@ies1xabia.org>
2019-03-24 17:38:59 +0100

    gaste el metode numVertex()

--- src/main/java/exercici02/Exercici02.java
+++ src/main/java/exercici02/Exercici02.java
@@ -15,11 +15,13 @@ public class Exercici02 {
         Poligon p1 = new Poligon(puntsPoligon);
         System.out.println("Els punts del polígon son: \n");
         p1.toString();
+        System.out.println("Te un total de: " + p1.numVertex() + " vertex.");
         System.out.println("El seu perímetre es: " + p1.perimetre(puntsPoligon) + "\n");
         System.out.println("Traslladem el polígon +4 en 'x' i -3 en 'y': ");
         p1.trasllada(4,-3);
         System.out.println("Els punts del polígon son: \n");
         p1.toString();
+        System.out.println("Te un total de: " + p1.numVertex() + " vertex.");
         System.out.println("El seu perímetre es: " + p1.perimetre(puntsPoligon));
 
     }
